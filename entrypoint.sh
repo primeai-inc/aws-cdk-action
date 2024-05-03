a9b17e62f0b106d84f40ca871af350a8f9f0ac2d
hmohndp <144478368+hmohndp@users.noreply.github.com>
2024-05-03 11:20:21 -0500

    Update entrypoint.sh
    Force pip to install and ignore the system packager

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [ -f "requirements.txt" ]; then
-  pip3 install -r requirements.txt
+  pip3 install -r requirements.txt --break-system-packages
 fi
 
 git config --global --add safe.directory /github/workspace
